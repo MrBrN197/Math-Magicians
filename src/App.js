14590f2b7e7986c0511bff48855d770c6ee15a19
brianorora <brianorora@gmail.com>
2021-10-18 20:47:46 +0300

    Use `parseInt` for `value` prop

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ class App extends react.Component {
     return (
       <Calculator
         updateDetails={this.updateDetails}
-        value={this.state.next || this.state.total}
+        value={parseInt(this.state.next || this.state.total, 10)}
       />
     );
   }
