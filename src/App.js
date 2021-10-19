9c1e5ce51f1e2082ffc4d5a045b8bb44406432c5
brianorora <brianorora@gmail.com>
2021-10-19 13:57:25 +0300

    Refactor `App` to functional component

--- src/App.js
+++ src/App.js
@@ -1,36 +1,29 @@
-import react from 'react';
+import { useState } from 'react';
 import Calculator from './components/Calculator';
 import calculate from './logic/calculate';
 
-class App extends react.Component {
-  constructor(props) {
-    super(props);
-    this.state = { total: null, next: null, operation: null };
-    this.updateDetails = this.updateDetails.bind(this);
+function App() {
+  const [state, setState] = useState({ total: null, next: null, operation: null });
+  const updateDetails = (e) => {
+    const newObj = calculate(state, e.target.textContent);
+    setState(newObj);
+  };
+
+  const { next, operation, total } = state;
+  const op = operation === '%' ? 'mod' : operation;
+  let result = '';
+  if (total) {
+    result = `${total} ${op || ''} ${next || ''}`;
+  } else if (next) {
+    result = `${next} ${op || ''}`;
   }
 
-  updateDetails(e) {
-    const newObj = calculate(this.state, e.target.textContent);
-    this.setState(newObj);
-  }
-
-  render() {
-    const { next, operation, total } = this.state;
-    const op = operation === '%' ? 'mod' : operation;
-    let result = '';
-    if (total) {
-      result = `${total} ${op || ''} ${next || ''}`;
-    } else if (next) {
-      result = `${next} ${op || ''}`;
-    }
-
-    return (
-      <Calculator
-        updateDetails={this.updateDetails}
-        value={result}
-      />
-    );
-  }
+  return (
+    <Calculator
+      updateDetails={updateDetails}
+      value={result}
+    />
+  );
 }
 
 export default App;
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import './Calculator.css';
 import PropTypes from 'prop-types';
 
