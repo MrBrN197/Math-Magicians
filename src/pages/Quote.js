87c3ea31311512c96d789ad084c836244e6d5c66
brianorora <brianorora@gmail.com>
2021-10-27 14:51:13 +0300

    Add content to `Quote` page

--- src/pages/Quote.js
+++ src/pages/Quote.js
@@ -1,7 +1,24 @@
-const Quote = () => (
-  <div>
-    <div>Quote</div>
-  </div>
-);
+import { useEffect, useState } from 'react';
+
+const Quote = () => {
+  const [state, setState] = useState({});
+
+  useEffect(() => {
+    const fetchQuote = async () => {
+      const response = await fetch('https://random-math-quote-api.herokuapp.com/');
+      const data = await response.json();
+      setState(data);
+    };
+    fetchQuote();
+    setTimeout(fetchQuote, 8000);
+  }, []);
+
+  return (
+    <div>
+      <h2>{state.author}</h2>
+      <div>{state.quote}</div>
+    </div>
+  );
+};
 
 export default Quote;
